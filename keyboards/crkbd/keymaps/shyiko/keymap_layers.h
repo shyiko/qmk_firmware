aa6e8e361e458a4124cf3d95ff6e3f0d04c54ac0
Stanley Shyiko <stanley.shyiko@gmail.com>
2020-09-29 19:35:50 -0700

    Update keymap

--- keyboards/crkbd/keymaps/shyiko/keymap_layers.h
+++ keyboards/crkbd/keymaps/shyiko/keymap_layers.h
@@ -4,6 +4,7 @@
 // _______ == KC_TRNS (transparent, i.e. pass-through to lower layer)
 // XXXXXXX == KC_NO (noop)
 
+// KC_PLUS/KC_MINUS
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L_QWERTY] = LAYOUT(                                                                                  \
@@ -22,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L_LOWER] = LAYOUT(                                                                                                            \
     KC_ESC,  KC_1,      KC_2,    KC_3,    KC_4,    KC_5,       /**/ KC_6,        KC_7,    KC_8,     KC_9,    KC_0,    _______,   \
-    _______, KC_LBRC,   KC_RBRC, KC_LPRN, KC_RPRN, KC_KP_PLUS, /**/ KC_KP_MINUS, KC_COLN, KC_EQUAL, KC_LCBR, KC_RCBR, KC_GRAVE,  \
+    _______, KC_LBRC,   KC_RBRC, KC_LCBR, KC_RCBR, KC_KP_PLUS, /**/ KC_KP_MINUS, KC_COLN, KC_EQUAL, KC_LPRN, KC_RPRN, KC_GRAVE,  \
     _______, KC_EXLM,   KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    /**/ KC_CIRC,     KC_AMPR, KC_ASTR,  _______, _______, KC_BSLASH, \
                                  _______, _______, _______,    /**/ _______,     _______, _______                                \
   ),
@@ -37,17 +38,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // F1-12 are on this layer (and not 1-12) due to KC_DELETE
   [L_RAISE] = LAYOUT(                                                                                                      \
-    _______, KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,   /**/ KC_F6,     KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_DELETE,   \
-    _______, KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX, /**/ KC_PGUP,   KC_HOME, KC_UP,   KC_END,   XXXXXXX, TG(L_MOUSE), \
-    _______, KC_Z,    KC_X,   KC_C,    KC_V,    XXXXXXX, /**/ KC_PGDOWN, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, _______,     \
-                              _______, _______, _______, /**/ _______,   _______, _______                                  \
+    _______, KC_F1,   KC_F2,  KC_F3,   KC_F4,   KC_F5,   /**/ KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,    KC_DELETE,   \
+    _______, KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, KC_HOME, KC_UP,   KC_END,   KC_PGUP,   TG(L_MOUSE), \
+    _______, KC_Z,    KC_X,   KC_C,    KC_V,    XXXXXXX, /**/ XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDOWN, _______,     \
+                              _______, _______, _______, /**/ _______, _______, _______                                    \
   ),
 
-  [L_ADJUST] = LAYOUT(                                                                                                 \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ _______, _______, KC_PGUP,   _______, _______, _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ _______, KC_HOME, KC_PGDOWN, KC_END,  _______, _______, \
-                               _______, _______, _______, /**/ _______, _______, _______                               \
+  [L_ADJUST] = LAYOUT(                                                                                                  \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX,  XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PLUS, /**/ KC_MINUS, _______, KC_PGUP,   _______, _______, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX,  KC_HOME, KC_PGDOWN, KC_END,  _______, _______, \
+                               _______, _______, _______, /**/ _______,  _______, _______                               \
   ),
 
   [L_EXTRA] = LAYOUT(                                                                                                        \

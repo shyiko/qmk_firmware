5d178cb0a6f5ec25b6fc6d42b6ab69c621dedbb9
Stanley Shyiko <stanley.shyiko@gmail.com>
2020-09-29 21:33:03 -0700

    Update keymap

--- keyboards/crkbd/keymaps/shyiko/keymap_layers.h
+++ keyboards/crkbd/keymaps/shyiko/keymap_layers.h
@@ -4,7 +4,9 @@
 // _______ == KC_TRNS (transparent, i.e. pass-through to lower layer)
 // XXXXXXX == KC_NO (noop)
 
-// KC_PLUS/KC_MINUS
+// L_LOWER contains KC_KP_PLUS and not KC_PLUS so that both + & = would register
+// when typed together (try typing += or =+ fast enough where + is KC_PLUS and
+// you'll see that second character is often lost).
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [L_QWERTY] = LAYOUT(                                                                                  \
@@ -21,11 +23,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          KC_LALT, LOWER, KC_SPC, /**/ KC_ENT, RAISE, KC_LGUI                            \
   ),
 
-  [L_LOWER] = LAYOUT(                                                                                                            \
-    KC_ESC,  KC_1,      KC_2,    KC_3,    KC_4,    KC_5,       /**/ KC_6,        KC_7,    KC_8,     KC_9,    KC_0,    _______,   \
-    _______, KC_LBRC,   KC_RBRC, KC_LCBR, KC_RCBR, KC_KP_PLUS, /**/ KC_KP_MINUS, KC_COLN, KC_EQUAL, KC_LPRN, KC_RPRN, KC_GRAVE,  \
-    _______, KC_EXLM,   KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    /**/ KC_CIRC,     KC_AMPR, KC_ASTR,  _______, _______, KC_BSLASH, \
-                                 _______, _______, _______,    /**/ _______,     _______, _______                                \
+  [L_LOWER] = LAYOUT(                                                                                                         \
+    KC_ESC,  KC_1,      KC_2,    KC_3,    KC_4,    KC_5,       /**/ KC_6,     KC_7,    KC_8,     KC_9,    KC_0,    _______,   \
+    _______, KC_LBRC,   KC_RBRC, KC_LCBR, KC_RCBR, KC_KP_PLUS, /**/ KC_MINUS, KC_COLN, KC_EQUAL, KC_LPRN, KC_RPRN, KC_GRAVE,  \
+    _______, KC_EXLM,   KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    /**/ KC_CIRC,  KC_AMPR, KC_ASTR,  _______, _______, KC_BSLASH, \
+                                 _______, _______, _______,    /**/ _______,  _______, _______                                \
   ),
 
   // NOTE: make sure to update keymap.c if you change this layer
@@ -44,11 +46,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               _______, _______, _______, /**/ _______, _______, _______                                    \
   ),
 
-  [L_ADJUST] = LAYOUT(                                                                                                  \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX,  XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PLUS, /**/ KC_MINUS, _______, KC_PGUP,   _______, _______, _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX,  KC_HOME, KC_PGDOWN, KC_END,  _______, _______, \
-                               _______, _______, _______, /**/ _______,  _______, _______                               \
+  [L_ADJUST] = LAYOUT(                                                                                                 \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PLUS, /**/ XXXXXXX, _______, KC_PGUP,   _______, _______, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ XXXXXXX, KC_HOME, KC_PGDOWN, KC_END,  _______, _______, \
+                               _______, _______, _______, /**/ _______, _______, _______                               \
   ),
 
   [L_EXTRA] = LAYOUT(                                                                                                        \
